418e455cbe7596e07cdac6aa70335f4290ae714b
Clayman <Clayman@DESKTOP-DM8E77U>
2022-06-12 19:26:00 +0300

    String

---
+++ IntroductionToOOP/String/main.cpp
@@ -0,0 +1,161 @@
+#include<iostream>
+using namespace std;
+
+class String
+{
+	int size;	//размер строки в Байтах
+	char* str;	//указатель на строку в динамической памяти
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
+	{
+		return str;
+	}
+
+
+	//				Constructors:
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefConstructor:\t" << this << endl;
+	}
+	String(const char* str)
+	{
+		this->size = strlen(str) + 1;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = str[i];
+		cout << "Constructor:\t" << this << endl;
+	}
+	String(const String& other)
+	{
+		this->size = other.size;
+		//Deep copy (Глубокое копирование, побитовое копирование, побайтовое/поэементное копирование)
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~String()
+	{
+		delete[] this->str;
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	//				Operators
+	String& operator=(const String& other)
+	{
+		if (this == &other)return *this;
+		delete[] this->str;
+		this->size = other.size;
+		//CopyAssignment, так же как и CopyConstructor выполняет DeepCopy
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	char operator[](int i)const//i - index
+	{
+		return str[i];
+	}
+	char& operator[](int i)//i - index
+	{
+		return str[i];
+	}
+
+
+	//				Methods:
+	void print()const
+	{
+		cout << "Size:\t" << size << endl;
+		cout << "Str:\t" << str << endl;
+	}
+};
+String operator+(const String& left, const String& right)
+{
+	//Конкатенация строк
+	String cat(left.get_size() + right.get_size() - 1);
+	for (int i = 0; i < left.get_size(); i++)
+		cat[i] = left[i];
+	//l-value = r-value;
+		//cat.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		cat[i + left.get_size() - 1] = right[i];
+		//cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+	return cat;
+}
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
+//#define NULL_TERMINATED_LINES
+//#define DEBUG_ASSERTION_FAILED_1
+//#define DEBUG_ASSERTION_FAILED_2
+//#define CONSTRUCTORS_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef NULL_TERMINATED_LINES
+	//ASCII-символ с кодом 0 '\0'
+//NULL Terminated lines
+//char str[] = { 'H', 'e', 'l', 'l', 'o', 0 };
+	char str[] = "Hello";
+	cout << str << endl;
+	cout << sizeof(str) << endl;
+	cout << strlen(str) << endl;
+#endif // NULL_TERMINATED_LINES
+
+#ifdef DEBUG_ASSERTION_FAILED_1
+	int a = 2;
+	int* pa = &a;
+	int arr[] = { 3,5,8 };
+
+	delete arr;
+#endif // DEBUG_ASSERTION_FAILED_1
+
+#ifdef DEBUG_ASSERTION_FAILED_2
+	int* arr = new int[5];
+	int* brr = arr;
+	delete[] brr;
+	delete[] arr;
+#endif // DEBUG_ASSERTION_FAILED_2
+
+#ifdef CONSTRUCTORS_CHECK
+	String str1;
+	str1.print();
+
+	String str2(44);
+	str2.print();
+
+	String str3 = "Hello";
+	str3 = str3;
+	str3.print();
+	cout << str3 << endl;
+
+	String str4 = str3;		//Copy constructor
+	cout << "Str4:\t" << str4 << endl;
+
+	String str5;	//Default constructor
+	str5 = str4;	//Copy assignment (оператор присваивания)
+	cout << str5 << endl;
+
+	int a = 2;
+	int b = 3;
+	a = b;
+#endif // CONSTRUCTORS_CHECK
+
+	String str1 = "Hello";
+	String str2 = "World";
+	//String str3 = str1 + " " + str2;
+	//cout << str3 << endl;
+	str1 += str2;
+	cout << str1 << endl;
+}
--- ReadMe.txt
+++ ReadMe.txt
@@ -2,9 +2,22 @@ https://github.com/okovtun/SPU_111_OOP.git
 https://www.youtube.com/watch?v=MQwW_YFhVCU&list=PLeqyOOqxeiIPy9kTEWJfeRfP1SlTccBPQ
 https://meet.google.com/mho-khdw-fhj
 
+TOREAD:
+https://cplusplus.com/doc/tutorial/classes2/
+
 TODO:
+1. Для класса String перегрузить оператор ввода:
+	String str;
+	cout << "Введите строку: "; cin >> str;
+2. Написать метод void to_upper(), который переводит строку в верхний регистр;
+3. Написать метод void to_lower(), который переводит строку в нижний регистр;
+4. Реализовать метод ??? is_palindrome(???), который опеределяет,
+   является ли строка палиндромом
+
+DONE:
 1. Проверчный код в секции CONVERSIONS_HOME_WORK должен заработать:
 	https://github.com/okovtun/SPU_111_OOP/blob/a434eb0cd56ac34d91ff59951c5d6d8e5b2afa61/IntroductionToOOP/Fraction/main.cpp#L361
+	https://www.webmath.ru/poleznoe/formules_12_7.php
 2. В Solution IntroductionToOOP добавить проект String и реализовать в нем класс String,
    описывающий строку. Обеспечить следующие варианты создания объектов:
    String str1;		//создает пустую строку размером 80 Byte
