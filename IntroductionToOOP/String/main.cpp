292d5252c1029ecdc584b71807982654ab33722d
Clayman <Clayman@DESKTOP-DM8E77U>
2022-06-19 18:18:30 +0300

    Methods out of class

--- IntroductionToOOP/String/main.cpp
+++ IntroductionToOOP/String/main.cpp
@@ -14,95 +14,115 @@ class String
 	int size;	//размер строки в Байтах
 	char* str;	//указатель на строку в динамической памяти
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
 
 
 	//				Constructors:
-	explicit String(int size = 80) :size(size), str(new char[size] {})
-	{
-		cout << "DefConstructor:\t" << this << endl;
-	}
-	String(const char* str) :String(strlen(str) + 1)
-	{
-		for (int i = 0; i < size; i++)this->str[i] = str[i];
-		cout << "Constructor:\t" << this << endl;
-	}
-	String(const String& other) :String(other.str)
-	{
-		//Deep copy (Глубокое копирование, побитовое копирование, побайтовое/поэементное копирование)
-		cout << "CopyConstructor:" << this << endl;
-	}
-	~String()
-	{
-		delete[] this->str;
-		cout << "Destructor:\t" << this << endl;
-	}
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	~String();
 
 	//				Operators
-	String& operator=(const String& other)
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
-		this->size = other.size;
-		//CopyAssignment, так же как и CopyConstructor выполняет DeepCopy
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
-	String& operator+=(const String& other)
-	{
-		return *this = *this + other;
-	}
-	char operator[](int i)const//i - index
-	{
-		return str[i];
-	}
-	char& operator[](int i)//i - index
-	{
-		return str[i];
-	}
-
+	String& operator=(const String& other);
+	String& operator+=(const String& other);
+	char operator[](int i)const;//i - index
+	char& operator[](int i);//i - index
 
 	//				Methods:
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
-	String& to_upper()
-	{
-		for (int i = 0; i < size; i++)
-		{
-			//if (str[i] >= 'a' && str[i] <= 'z')str[i] -= ' ';
-			//if (str[i] >= 'а' && str[i] <= 'я')str[i] -= ' ';
-			str[i] = ::toupper(str[i]);
-		}
-		return *this;
-	}
-	String& to_lower()
-	{
-		for (int i = 0; i < size; i++)
-		{
-			//if (str[i] >= 'A' && str[i] <= 'Z')str[i] += ' ';
-			//if (str[i] >= 'А' && str[i] <= 'Я')str[i] += ' ';
-			str[i] = tolower(str[i]);
-		}
-		return *this;
-	}
-
+	void print()const;
+	String& to_upper();
+	String& to_lower();
 };
+int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+
+
+//				Constructors:
+String::String(int size) :size(size), str(new char[size] {})
+{
+	cout << "DefConstructor:\t" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)
+{
+	for (int i = 0; i < size; i++)this->str[i] = str[i];
+	cout << "Constructor:\t" << this << endl;
+}
+String::String(const String& other) :String(other.str)
+{
+	//Deep copy (Глубокое копирование, побитовое копирование, побайтовое/поэементное копирование)
+	cout << "CopyConstructor:" << this << endl;
+}
+String::~String()
+{
+	delete[] this->str;
+	cout << "Destructor:\t" << this << endl;
+}
+
+//				Operators
+String& String::operator=(const String& other)
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	this->size = other.size;
+	//CopyAssignment, так же как и CopyConstructor выполняет DeepCopy
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyAssignment:\t" << this << endl;
+	return *this;
+}
+String& String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+char String::operator[](int i)const//i - index
+{
+	return str[i];
+}
+char& String::operator[](int i)//i - index
+{
+	return str[i];
+}
+
+
+//				Methods:
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
+String& String::to_upper()
+{
+	for (int i = 0; i < size; i++)
+	{
+		//if (str[i] >= 'a' && str[i] <= 'z')str[i] -= ' ';
+		//if (str[i] >= 'а' && str[i] <= 'я')str[i] -= ' ';
+		str[i] = ::toupper(str[i]);
+	}
+	return *this;
+}
+String& String::to_lower()
+{
+	for (int i = 0; i < size; i++)
+	{
+		//if (str[i] >= 'A' && str[i] <= 'Z')str[i] += ' ';
+		//if (str[i] >= 'А' && str[i] <= 'Я')str[i] += ' ';
+		str[i] = tolower(str[i]);
+	}
+	return *this;
+}
 String operator+(const String& left, const String& right)
 {
 	//Конкатенация строк
