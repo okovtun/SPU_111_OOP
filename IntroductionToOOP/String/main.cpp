a5afced47475255932f9ffb6df6bc40b3adf4e05
Clayman <Clayman@DESKTOP-DM8E77U>
2022-06-19 16:51:32 +0300

    Input and Calling constructors.

--- IntroductionToOOP/String/main.cpp
+++ IntroductionToOOP/String/main.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
+#include<ctype.h>
+#include<Windows.h>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+class String;
+String operator+(const String& left, const String& right);
 
 class String
 {
@@ -60,6 +68,10 @@ public:
 		cout << "CopyAssignment:\t" << this << endl;
 		return *this;
 	}
+	String& operator+=(const String& other)
+	{
+		return *this = *this + other;
+	}
 	char operator[](int i)const//i - index
 	{
 		return str[i];
@@ -76,6 +88,27 @@ public:
 		cout << "Size:\t" << size << endl;
 		cout << "Str:\t" << str << endl;
 	}
+	String& to_upper()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			//if (str[i] >= 'a' && str[i] <= 'z')str[i] -= ' ';
+			//if (str[i] >= 'а' && str[i] <= 'я')str[i] -= ' ';
+			str[i] = ::toupper(str[i]);
+		}
+		return *this;
+	}
+	String& to_lower()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			//if (str[i] >= 'A' && str[i] <= 'Z')str[i] += ' ';
+			//if (str[i] >= 'А' && str[i] <= 'Я')str[i] += ' ';
+			str[i] = tolower(str[i]);
+		}
+		return *this;
+	}
+	
 };
 String operator+(const String& left, const String& right)
 {
@@ -87,18 +120,37 @@ String operator+(const String& left, const String& right)
 		//cat.get_str()[i] = left.get_str()[i];
 	for (int i = 0; i < right.get_size(); i++)
 		cat[i + left.get_size() - 1] = right[i];
-		//cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+	//cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];
 	return cat;
 }
 std::ostream& operator<<(std::ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
+std::istream& operator>>(std::istream& is, String& obj)
+{
+	//const int SIZE = USHRT_MAX;
+	char sz_buffer[USHRT_MAX] = {};	//sz - String Zero (Строка, заканчивающаяся нулем)
+	SetConsoleCP(1251);
+	is >> sz_buffer;
+	SetConsoleCP(866);
+	obj = sz_buffer;
+	return is;
+}
+std::istream& getline(std::istream& is, String& obj)
+{
+	SetConsoleCP(1251);
+	is.getline(obj.get_str(), obj.get_size());
+	SetConsoleCP(866);
+	return is;
+}
 
 //#define NULL_TERMINATED_LINES
 //#define DEBUG_ASSERTION_FAILED_1
 //#define DEBUG_ASSERTION_FAILED_2
 //#define CONSTRUCTORS_CHECK
+//#define OPERATOR_PLUS_CHECK
+//#define KEYBOARD_INPUT_CHECK
 
 void main()
 {
@@ -152,10 +204,41 @@ void main()
 	a = b;
 #endif // CONSTRUCTORS_CHECK
 
+#ifdef OPERATOR_PLUS_CHECK
 	String str1 = "Hello";
 	String str2 = "World";
 	//String str3 = str1 + " " + str2;
 	//cout << str3 << endl;
 	str1 += str2;
 	cout << str1 << endl;
+#endif // OPERATOR_PLUS_CHECK
+
+#ifdef KEYBOARD_INPUT_CHECK
+	String str;
+	cout << "Введите строку: ";
+	//cin >> str;
+	getline(cin, str);
+	cout << str << endl;
+	str.to_upper();
+	cout << str << endl;
+	str.to_lower();
+	cout << str << endl;
+
+#endif // KEYBOARD_INPUT_CHECK
+
+	String str1;	//Default constructor
+	str1.print();
+	String str2(22);//Single-argument constructor
+	str2.print();
+	String str3 = "Hello";//Single-argument constructor
+	str3.print();
+	String str4();	//Здесь не вызывается никакой конструктор, 
+	//потому что здесь НЕ создается объект,
+	//а объявляется функция str4(), которая НЕ принимает никаких параметров, 
+	//и возвращает значение типа String.
+	//str4.
+	//А конструктор по умолчанию можно явно вызвать так:
+	String str5{};	//Default constructor
+	str5.print();
+	//При помощи фигурных скобок можно вызвать любой конструктор.
 }
